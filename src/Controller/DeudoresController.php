b8090a40bcd67f94965e5a4872d4cc507801fe1f
nicoq07 <nicolasquiroga@MacBook-Pro-de-Nicolas.local>
2017-03-21 23:20:18 -0300

    Permisos bug

--- src/Controller/DeudoresController.php
+++ src/Controller/DeudoresController.php
@@ -12,14 +12,14 @@ class DeudoresController extends AppController
 {
     public function isAuthorized($user)
     {
-        if(isset($user['role_id']) and $user['role_id'] == 3)
+        if(isset($user['role_id']) && $user['role_id'] == 3)
         {
             if(in_array($this->request->action, ['view','index']))
             {
                 return true;
             }
         }
-        elseif (isset($user['role_id']) and $user['role_id'] == 2) {
+        elseif (isset($user['role_id']) && $user['role_id'] == 2) {
             if(in_array($this->request->action, ['edit','add','view','index']))
             {
                 return true;
